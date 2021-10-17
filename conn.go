a36a11a378678a9c6780415d607b1b997125f9ce
Reinaldo Pineda <Reinaldo.Pineda@focusservices.com>
2021-10-17 16:57:52 -0600

    setting up the pg connection

---
+++ conn.go
@@ -0,0 +1,44 @@
+package conn
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "root"
+	dbname   = "library"
+)
+
+func getConnection() (*sql.DB, error) {
+
+	psCredentials := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psCredentials)
+
+	return db, err
+}
+
+/*
+func main() {
+
+	db, err := getConnection()
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("\nSuccessfully connected to database!")
+}
+*/
